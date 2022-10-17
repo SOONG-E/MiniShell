5f1067df8f1ff6803ef6636c69e3e0534cd37ca2
soong-e <sksuwjd@naver.com>
2022-10-17 17:20:00 +0900

    md funcs

--- execute.c
+++ execute.c
@@ -2,24 +2,24 @@
 
 static int	check_exit_code(void)
 {
-	if (ft_strlen(g_info->exit_code) == 1 && g_info->exit_code[0] == '0')
+	if (!ft_strcmp(g_info->exit_code, "0"))
 		return (0);
 	else
 		return (1);
 }
 
-void	search_tree(t_token	*tree)
+void	execute_tree(t_token *tree)
 {
 	if (!tree)
 		return ;
 	if (tree->left)
-		search_tree(tree->left);
+		execute_tree(tree->left);
 	if	(tree->symbol->type == T_AND_IF || tree->symbol->type == T_OR_IF)
 	{
 		if ((tree->symbol->type == T_AND_IF && !check_exit_code())
 			|| (tree->symbol->type == T_OR_IF && check_exit_code()))
-			search_tree(tree->right);
+			execute_tree(tree->right);
 	}
-//	else
-		// excute_pipe_line(tree->symbol);
+	else
+		execute_pipe_line(tree->symbol);
 }
